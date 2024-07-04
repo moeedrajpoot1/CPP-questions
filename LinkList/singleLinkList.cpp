f18c86a713cbdd5f6955cb9f662a82cabee70dd8
moeedrajpoot1 <moeedrajpoot1@gmail.com>
2024-07-04 15:47:03 +0500

    insertion

--- LinkList/singleLinkList.cpp
+++ LinkList/singleLinkList.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>
 using namespace std;
-
+// Insertation in th headdddddddddddddddddddddddddddddddddddddddddd
 // class Node{
 //     public:
 //     int data;
@@ -41,6 +41,50 @@ using namespace std;
 // }
 
 //
+
+
+// Inserting At taaaaaaaaaaaaiiiiiiiiiiiiiiiiLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+ 
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     public:
+//     Node(int val){
+//         this->data=val;
+//         this->next=nullptr;
+//     }
+// };
+
+
+// void AddatEnd(Node* &tail,int d){
+// Node* temp= new Node(d);
+// tail->next=temp;
+// tail=tail->next;
+
+
+// };
+
+//  void PrintLinkedList(Node* &head){
+// Node* temp=head;
+// while(temp!=nullptr){
+//     cout<<temp->data<<" ";
+//     temp=temp->next;
+
+// }
+//  };
+
+//  int main(){
+//     Node* node1=new Node(200);
+//     Node* head=node1;
+//     Node* tail=node1;
+//     PrintLinkedList(head);
+//     AddatEnd(tail,30);
+//     PrintLinkedList(head);
+
+//  }
+
 
 
 
