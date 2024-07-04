a7d957a3d9b8393830f07b1861894159c533708c
moeedrajpoot1 <moeedrajpoot1@gmail.com>
2024-07-04 14:27:04 +0500

    linked list

---
+++ LinkList/singleLinkList.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     public:
+//     Node(int val){
+//         this->data=val;
+//         this->next=nullptr;
+//     }
+// };
+
+// void insertatHead(Node* &head,int d){
+// // New Node Create
+//  Node* temp=new Node(d);
+//  temp->next=head;
+//  head=temp;
+// };
+
+// void PrintLinkedList(Node* &head){
+// Node* temp=head;
+// while(temp!=nullptr){
+//     cout<<temp->data<<" ";
+//     temp=temp->next;
+
+// }
+// cout<<endl;
+// };
+// int main(){
+// Node* node1= new Node(10);
+// cout<<node1->data<<endl;
+// cout<<node1->next;
+// Node* head=node1;
+// PrintLinkedList(head);
+// insertatHead(head,12);
+// PrintLinkedList(head);
+// }
+
+//
+
+
+
